743f7cafdcc87b4329cf9cab1de70c30a0ecb14b
Patricia <pa1809ty@gmail.com>
2016-01-04 11:00:00 +0000

    Add cucumber test for a charity signs in

---
+++ features/signing_in.feature
@@ -0,0 +1,11 @@
+Feature: Signing in
+
+Background:
+  Given the charity is in the sign in page
+    And the charity is already registered
+
+Scenario: A charity signs in
+  When the charity fills the valid email and password
+    And the charity signs in
+  Then the charity is taken to the homepage
+    And the charity sees a notice "You have successfully signed in"
---
+++ features/step_definitions/signing_in_steps.rb
@@ -0,0 +1,26 @@
+Given(/^the charity is in the sign in page$/) do
+  visit new_charity_session_path
+end
+
+Given(/^the charity is already registered$/) do
+  @email = Faker::Internet.email
+  Charity.create!(:email => @email, :password => "password")
+end
+
+When(/^the charity fills the valid email and password$/) do
+  fill_in 'Email', :with => @email
+  fill_in 'Password', :with => "password"
+end
+
+When(/^the charity signs in$/) do
+  click_button 'Log in'
+end
+
+Then(/^the charity is taken to the homepage$/) do
+  expect(current_path).to eq(root_path)
+end
+
+Then(/^the charity sees a notice "(.*?)"$/) do |message|
+  expect(page.has_content?(message)).to be true
+end
+
