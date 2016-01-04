28811d230d6ed789d1f1b78bbc9039f0da967444
Patricia <pa1809ty@gmail.com>
2016-01-04 19:59:18 +0000

    Modify routes removing charity

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :charities
   root to: "home#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
