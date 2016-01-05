3a2e4279d862af9887e7c9a51e3a316dccf90ec8
Patricia <pa1809ty@gmail.com>
2016-01-05 17:21:34 +0000

    Add test for relationship between address and user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,11 +5,17 @@ RSpec.describe User, type: :model do
     before do
       @email = Faker::Internet.email
       @user = User.create!(email: @email, password: "password", profile: Profile.new(organization_name: "Food for everyone"))
+      @address = Address.create!(door_number: "35", street: "Pelham Road", city: "London", country: "UK", postcode: "N16 0NM", user: @user)
     end
 
     it "has one profile when it created" do
       expect(User.first.profile.organization_name).to eq("Food for everyone")
       expect(Profile.all.length).to eq(1)
+    end
+
+    it "has one address" do
+      expect(User.first.address.street).to eq("Pelham Road")
+      expect(User.first.address.city).to eq("London")
     end
   end
 end
