fbb75c60b5f0d5bad7cdecd407b57f152dd0fef2
Patricia <pa1809ty@gmail.com>
2016-01-05 17:44:31 +0000

    Add test to see user address from profile

--- spec/models/profile_spec.rb
+++ spec/models/profile_spec.rb
@@ -7,10 +7,15 @@ RSpec.describe Profile, type: :model do
       @email = Faker::Internet.email
       @charity = Charity.create!(email: @email, password: "password")
       @profile = Profile.create!(organization_name: "Food for everyone", user: @charity)
+      @address = Address.create!(door_number: "35", street: "Pelham Road", city: "London", country: "UK", postcode: "N16 0NM", user: @charity)
     end
 
     it "belongs to a user" do
       expect(Profile.first.user.email).to eq(@email)
+    end
+
+    it "displays the user address" do
+      expect(Profile.first.user.address.street).to eq("Pelham Road")
     end
   end
 end
