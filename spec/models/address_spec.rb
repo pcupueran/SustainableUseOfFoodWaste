852cd7234659fd2287f6f80d968347214cef6c85
Patricia <pa1809ty@gmail.com>
2016-01-06 10:33:53 +0000

    Rspec tests pass for the relationship between profile and address

--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -5,11 +5,12 @@ RSpec.describe Address, type: :model do
     before do
       @email = Faker::Internet.email
       @charity = Charity.create!(email: @email, password: "password")
-      @address = Address.create!(door_number: "35", street: "Pelham Road", city: "London", country: "UK", postcode: "N16 0NM", user: @charity)
+      @profile = Profile.create!(organization_name: "Food for everyone", user: @charity)
+      @address = Address.create!(door_number: "35", street: "Pelham Road", city: "London", country: "UK", postcode: "N16 0NM", profile: @profile)
     end
 
-    it "belongs to a user" do
-      expect(Address.first.user.email).to eq(@charity.email)
+    it "belongs to a profile" do
+      expect(Address.first.profile.organization_name).to eq(@profile.organization_name)
     end
   end
 end
--- spec/models/profile_spec.rb
+++ spec/models/profile_spec.rb
@@ -7,15 +7,16 @@ RSpec.describe Profile, type: :model do
       @email = Faker::Internet.email
       @charity = Charity.create!(email: @email, password: "password")
       @profile = Profile.create!(organization_name: "Food for everyone", user: @charity)
-      @address = Address.create!(door_number: "35", street: "Pelham Road", city: "London", country: "UK", postcode: "N16 0NM", user: @charity)
+      @address = Address.create!(door_number: "35", street: "Pelham Road", city: "London", country: "UK", postcode: "N16 0NM", profile: @profile)
     end
 
     it "belongs to a user" do
       expect(Profile.first.user.email).to eq(@email)
     end
 
-    it "displays the user address" do
-      expect(Profile.first.user.address.street).to eq("Pelham Road")
+    it "has one address" do
+      expect(Profile.first.address.street).to eq("Pelham Road")
+      expect(Profile.first.address.city).to eq("London")
     end
   end
 end
