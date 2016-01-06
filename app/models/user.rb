015ad70093caaf2881e4e4e8a378288e3117b603
Patricia <pa1809ty@gmail.com>
2016-01-06 10:31:03 +0000

    Delete relationship between user and address and add relationship between profile and address

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  has_one :address
   has_one :profile
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160105170954) do
+ActiveRecord::Schema.define(version: 20160106082015) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 20160105170954) do
     t.string   "city"
     t.string   "country"
     t.string   "postcode"
-    t.integer  "user_id"
+    t.integer  "profile_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
