2220f10f07a8c9010d61d229a12dcb6d61133d0f
Patricia <pa1809ty@gmail.com>
2016-01-06 17:34:17 +0000

    Add validation presence true for profile in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  validates :profile, presence: true
   has_one :profile
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,12 +2,14 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe "User" do
-    before do
+    it "raises an exception if profile not present" do
       @email = Faker::Internet.email
-      @user = User.create!(email: @email, password: "password", profile: Profile.new(organization_name: "Food for everyone"))
+      expect{ User.create!(email: @email, password: "password") }.to raise_error("Validation failed: Profile can't be blank")
     end
 
-    it "has one profile when it created" do
+    it "has one profile when it is created" do
+      @email = Faker::Internet.email
+      @user = User.create!(email: @email, password: "password", profile: Profile.new(organization_name: "Food for everyone"))
       expect(User.first.profile.organization_name).to eq("Food for everyone")
       expect(Profile.all.length).to eq(1)
     end
