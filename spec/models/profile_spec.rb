0633b72f429fdeedf1d36d52e12df8b09ec7e1b3
Patricia <pa1809ty@gmail.com>
2016-01-06 17:21:30 +0000

    Refactor test and add provider's profile has an address

--- spec/models/profile_spec.rb
+++ spec/models/profile_spec.rb
@@ -3,20 +3,40 @@ require 'rails_helper'
 RSpec.describe Profile, type: :model do
 
   describe "Profile" do
-    before do
-      @email = Faker::Internet.email
-      @charity = Charity.create!(email: @email, password: "password")
-      @profile = Profile.create!(organization_name: "Food for everyone", user: @charity)
-      @address = Address.create!(door_number: "35", street: "Pelham Road", city: "London", country: "UK", postcode: "N16 0NM", profile: @profile)
+    describe "Charity" do
+      before do
+        @email = Faker::Internet.email
+        @charity = Charity.create!(email: @email, password: "password")
+        @profile = Profile.create!(organization_name: "Food for everyone", user: @charity)
+        @address = Address.create!(door_number: "35", street: "Pelham Road", city: "London", country: "UK", postcode: "N16 0NM", profile: @profile)
+      end
+
+      it "belongs to a user" do
+        expect(Profile.first.user.email).to eq(@email)
+      end
+
+      it "has one address" do
+        expect(Profile.first.address.street).to eq("Pelham Road")
+        expect(Profile.first.address.postcode).to eq("N16 0NM")
+      end
     end
 
-    it "belongs to a user" do
-      expect(Profile.first.user.email).to eq(@email)
+    describe "Provider" do
+      before do
+        @email_provider = Faker::Internet.email
+        @provider = Provider.create!(email: @email_provider, password: "password")
+        @profile_provider = Profile.create!(organization_name: "Pret a manger", user: @provider)
+        @address_provider = Address.create!(door_number: "3", street: "Oxford Street", city: "London", country: "UK", postcode: "SW16 4RN", profile: @profile_provider)
+      end
+      it "belongs to a user" do
+        expect(Profile.first.user.email).to eq(@email_provider)
+      end
+
+      it "has one address" do
+        expect(Profile.first.address.street).to eq("Oxford Street")
+        expect(Profile.first.address.postcode).to eq("SW16 4RN")
+      end
     end
 
-    it "has one address" do
-      expect(Profile.first.address.street).to eq("Pelham Road")
-      expect(Profile.first.address.city).to eq("London")
-    end
   end
 end
