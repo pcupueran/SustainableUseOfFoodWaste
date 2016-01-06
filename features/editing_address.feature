faeb7c4e9ab6e9f562e3f2190355fd80da232fd6
Patricia <pa1809ty@gmail.com>
2016-01-06 17:12:04 +0000

    Add cucumber test for provider edits profile

--- features/editing_address.feature
+++ features/editing_address.feature
@@ -1,10 +1,15 @@
 Feature: Edit address
 
-Background:
-  Given a profile exist
+Scenario: charity edits address in its profile
+  Given a profile for charity exist
     And a user is in the editing profile page
+  When a user changes the address information
+    And a user updates his/her profile
+  Then the profile's address is updated
 
-Scenario: User edits address in his/her profile
+Scenario: provider edits address in its profile
+  Given a profile for provider exist
+    And a user is in the editing profile page
   When a user changes the address information
     And a user updates his/her profile
   Then the profile's address is updated
--- features/step_definitions/editing_address_steps.rb
+++ features/step_definitions/editing_address_steps.rb
@@ -1,8 +1,14 @@
-Given(/^a profile exist$/) do
+Given(/^a profile for charity exist$/) do
   @email = Faker::Internet.email
   @charity = Charity.create!(email: @email, password: "password")
   @profile = Profile.create!(organization_name: "Food for everyone", user: @charity)
   @address = Address.create!(door_number: "35", street: "Pelham Road", city: "London", country: "UK", postcode: "N16 0NM", profile: @profile)
+end
+Given(/^a profile for provider exist$/) do
+  @email = Faker::Internet.email
+  @provider = Provider.create!(email: @email, password: "password")
+  @profile = Profile.create!(organization_name: "Pret a manger", user: @provider)
+  @address = Address.create!(door_number: "3", street: "Tottenham High Road", city: "London", country: "UK", postcode: "SW16 4RN", profile: @profile)
 end
 
 Given(/^a user is in the editing profile page$/) do
@@ -21,4 +27,7 @@ Then(/^the profile's address is updated$/) do
   expect(Profile.first.address.street).to eq("Oxford Street")
 end
 
+Given(/^a provider is in the editing profile page$/) do
+  visit edit_profile_path(@profile_provider)
+end
 
