b49967b3186059a9f74daa9bda12d466bedfe10b
Patricia <pa1809ty@gmail.com>
2016-01-06 21:47:05 +0000

    Add relationship between booking and charity

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,10 +1,12 @@
 class Booking < ActiveRecord::Base
+  belongs_to :charity
   belongs_to :contribution
   after_save :notify_provider
 
   private
   def notify_provider
+    @charity = self.charity
     @provider = self.contribution.provider
-    UserNotification.notification(@provider).deliver_now
+    UserNotification.notification(@provider, @charity).deliver_now
   end
 end
