3fe61c7dfc0d4a4d1d4fa761bbdc42d89eab23e1
Patricia <pa1809ty@gmail.com>
2016-01-06 21:48:20 +0000

    Test notification method from UserNotification mailer

--- spec/mailers/user_notification_spec.rb
+++ spec/mailers/user_notification_spec.rb
@@ -1,5 +1,25 @@
 require "rails_helper"
 
 RSpec.describe UserNotification, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "notification" do
+    let(:mail) { UserNotification.notification(@provider, @charity)}
+
+    before do
+      @email = Faker::Internet.email
+      @profile = Profile.new(organization_name: "Pret a manger")
+      @provider = Provider.create!(email: @email, password: "password", profile: @profile)
+    end
+
+    it "has as subject" do
+      expect(mail.subject).to eq("The following charity has booked this contribution")
+    end
+
+    it "has as recipient" do
+      expect(mail.to).to eq([@provider.email])
+    end
+
+    it "has as sender" do
+      expect(mail.from).to eq(["usingfoodwaste@org.uk"])
+    end
+  end
 end
