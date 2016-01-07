485943247673f15ca1d4c1890423a0d041a719c8
Patricia <pa1809ty@gmail.com>
2016-01-07 13:49:13 +0000

    Refactor rspec adding user type to rspec test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,15 +2,23 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe "User" do
-    it "raises an exception if profile not present" do
+    it "raises an exception if type not present" do
       @email = Faker::Internet.email
-      expect{ User.create!(email: @email, password: "password") }.to raise_error("Validation failed: Profile can't be blank")
+      expect{ User.create!(email: @email, password: "password") }.to raise_error("Validation failed: Type can't be blank")
+    end
+
+    it "creates a user and a profile if valid data is provided" do
+      @email = Faker::Internet.email
+      @user = User.create!(email: @email, password: "password", type: "Charity")
+      expect(User.all.length).to eq(1)
+      expect(User.first.email).to eq(@email)
+      expect(User.first.type).to eq("Charity")
     end
 
     it "has one profile when it is created" do
       @email = Faker::Internet.email
-      @user = User.create!(email: @email, password: "password", profile: Profile.new(organization_name: "Food for everyone"))
-      expect(User.first.profile.organization_name).to eq("Food for everyone")
+      @user = User.create!(email: @email, password: "password", type: "Charity")
+      expect(User.first.profile).not_to be_nil
       expect(Profile.all.length).to eq(1)
     end
   end
