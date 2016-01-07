2059e41aa64649cf1e62ea5c7116da7a154d14c8
Patricia <pa1809ty@gmail.com>
2016-01-07 09:55:28 +0000

    Refactor user model remove profile validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,14 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  validates :profile, presence: true
+
   has_one :profile
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  after_create :create_profile
+
+  private
+  def create_profile
+    Profile.create!(user: self)
+  end
 end
