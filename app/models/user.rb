bd8c7cf1795815e898863aaa5b04d934ed6fb250
Patricia <pa1809ty@gmail.com>
2016-01-07 18:16:41 +0000

    Validate organization_name attribute of user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-
+  validates :organization_name, presence: true
   has_one :profile
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
