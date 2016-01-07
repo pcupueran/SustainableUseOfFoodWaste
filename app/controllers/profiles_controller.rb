70c8cfa15e0d8a2e57ae69373ee54356a49e64c1
Patricia <pa1809ty@gmail.com>
2016-01-07 18:16:09 +0000

    Remove binding.pry

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,14 +2,13 @@ class ProfilesController < ApplicationController
   before_filter :load_profile
 
   def update
-    binding.pry
-    @profile.update(profile_params)
+    @profile.update!(profile_params)
     redirect_to profile_path(@profile)
   end
 
   private
   def profile_params
-    params.require(:profile).permit(:user_attributes => [:id, :organization_name],:address_attributes => [:id, :door_number, :street, :city, :country, :postcode])
+    params.require(:profile).permit(:user_attributes => [:id, :organization_name, :email],:address_attributes => [:id, :door_number, :street, :city, :country, :postcode])
   end
 
   def load_profile
