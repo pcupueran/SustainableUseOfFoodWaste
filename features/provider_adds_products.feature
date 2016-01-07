c5b531662f80e201132d4f7472345e33c37be3b4
Patricia <pa1809ty@gmail.com>
2016-01-07 20:16:36 +0000

    Add cucumber test for provider creates a contribution with products

---
+++ features/provider_adds_products.feature
@@ -0,0 +1,11 @@
+Feature: Provider creates a contribution and adds products
+
+Scenario: Provider creates a contribution
+  Given a profile for provider exist
+    And a user is signed in
+  When a provider adds contribution
+  Then the page to add a contribution is shown
+  When a provider fills the valid data
+    And a provider creates contribution
+  Then a contribution is created
+    And the flash message "A contribution has been created" is shown
---
+++ features/step_definitions/provider_adds_products_steps.rb
@@ -0,0 +1,36 @@
+When(/^a provider adds contribution$/) do
+  click_on "Add contribution"
+end
+
+Then(/^the page to add a contribution is shown$/) do
+  expect(current_path).to eq(new_user_contribution_path)
+  expect(page).to have_content("Your contribution helps")
+  expect(page).to have_content("Products")
+end
+
+When(/^a provider fills the valid data$/) do
+  click_button "Add a product"
+  fill_in "Quantity", :with => 3
+  fill_in "Product Name", :with => "Tuna cans"
+  check "Perishable"
+  click_button "Add a product"
+  fill_in "Quantity", :with => 5
+  fill_in "Product Name", :with => "Chicken sandwiches"
+end
+
+When(/^a provider creates contribution$/) do
+  click_button "Add contribution"
+end
+
+Then(/^a contribution is created$/) do
+  expect(Contribution.all.length).to eq(1)
+  expect(Contribution.first.products.first.quantity).to eq(3)
+  expect(Contribution.first.products.first.product_name).to eq("Tuna cans")
+  expect(Contribution.first.products.first.perishable).to eq(false)
+
+  expect(Contribution.first.products.second.quantity).to eq(5)
+  expect(Contribution.first.products.second.product_name).to eq("Chicken sandwiches")
+  expect(Contribution.first.products.second.perishable).to eq(true)
+  expect(Product.all.length).to eq(2)
+end
+
