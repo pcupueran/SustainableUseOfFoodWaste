65c8fdae6272fb5d9b890a1fa6689fd947f18098
Patricia <pa1809ty@gmail.com>
2016-01-08 15:58:17 +0000

    add contribution model and controller

---
+++ app/controllers/contributions_controller.rb
@@ -0,0 +1,24 @@
+class ContributionsController < ApplicationController
+  def new
+    @contribution = Contribution.new
+  end
+
+  def create
+    binding.pry
+    @contribution = Contribution.new(user_id: params[:user_id])
+    @contribution.contribution_products.build
+    @contribution.assign_attributes(contribution_params)
+    @contribution.save!
+    redirect_to user_contribution_path(@contribution)
+  end
+
+  def add_product
+    binding.pry
+    Product.new
+  end
+
+  private
+  def contribution_params
+    params.require(:contribution).permit(:contribution_products_attributes => [:product_attributes => [:quantity, :product_name, :perishable]])
+  end
+end
--- app/models/contribution.rb
+++ app/models/contribution.rb
@@ -1,4 +1,7 @@
 class Contribution < ActiveRecord::Base
   has_one :booking
-  belongs_to :provider
+  belongs_to :user
+  has_many :contribution_products
+  has_many :products, :through => :contribution_products
+  accepts_nested_attributes_for :contribution_products
 end
