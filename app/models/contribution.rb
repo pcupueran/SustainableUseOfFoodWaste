f44dc57c48e93040dc3681b48390abd75712e200
Patricia <pa1809ty@gmail.com>
2016-01-08 16:45:18 +0000

    modify relationships between contribution and product

--- app/models/contribution.rb
+++ app/models/contribution.rb
@@ -1,7 +1,6 @@
 class Contribution < ActiveRecord::Base
   has_one :booking
   belongs_to :user
-  has_many :contribution_products
-  has_many :products, :through => :contribution_products
-  accepts_nested_attributes_for :contribution_products
+  has_many :products
+  accepts_nested_attributes_for :products
 end
