e3082898cd55e4498bb195de7e032742a776c17d
Patricia <pa1809ty@gmail.com>
2016-01-10 16:11:52 +0000

    Call geocode method before_saving so coordinates are up to date when creating or updating address

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,6 +1,7 @@
 class Address < ActiveRecord::Base
   belongs_to :profile
   geocoded_by :geocode_string
+  before_save :geocode, if: lambda { |address| address.changed? }
 
   def geocode_string
     "#{door_number} #{street}, #{postcode}, #{city}, #{country}"
