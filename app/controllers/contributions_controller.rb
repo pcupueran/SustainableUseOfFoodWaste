be628f83fec9d3e19faf6ab57edca360014751cd
Patricia <pa1809ty@gmail.com>
2016-01-11 17:12:09 +0000

    Use near from geocoder implemented in the model address to find the contributions which are within a certain distance

--- app/controllers/contributions_controller.rb
+++ app/controllers/contributions_controller.rb
@@ -29,6 +29,18 @@ class ContributionsController < ApplicationController
     Product.new
   end
 
+  def search
+    @contributions = []
+    @addresses =Address.joins(:profile => :user).where("users.type = ?", "Provider")
+    unless params[:distance].nil?
+      @addresses = @addresses.near([current_user.profile.address.latitude, current_user.profile.address.longitude], params[:distance], :units => :km)
+    end
+    @addresses.each do |address|
+      @contr = Contribution.joins(:user).where("users.id = ?", address.profile.user_id)
+      @contributions << @contr[0]
+    end
+  end
+
   private
   def contribution_params
     params.require(:contribution).permit(:collection_date, :collection_time, :products_attributes => [:quantity, :product_name, :perishable])
