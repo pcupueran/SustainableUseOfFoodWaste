eb0b1505ca279c798630f72b12c3fffd3ea33762
Patricia <pa1809ty@gmail.com>
2016-01-11 19:27:56 +0000

    Change callback geocode to update latitude and longitude before updating the record in the address table

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,7 +1,7 @@
 class Address < ActiveRecord::Base
   belongs_to :profile
   geocoded_by :geocode_string
-  before_save :geocode, if: lambda { |address| address.changed? }
+  before_update :geocode, if: lambda { |address| address.changed? }
 
   def geocode_string
     "#{door_number} #{street}, #{postcode}, #{city}, #{country}"
