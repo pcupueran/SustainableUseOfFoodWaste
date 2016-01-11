60e5cbc56ce0270c8a67ff0652007b51a8fcf115
Patricia <pa1809ty@gmail.com>
2016-01-11 17:13:51 +0000

    Create an address when a profile is created

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,6 +1,12 @@
 class Profile < ActiveRecord::Base
   has_one :address
   belongs_to :user
-  accepts_nested_attributes_for :address
-  accepts_nested_attributes_for :user
+  accepts_nested_attributes_for :address, :user
+
+  after_create :create_address
+
+  private
+  def create_address
+    Address.create!(profile: self)
+  end
 end
