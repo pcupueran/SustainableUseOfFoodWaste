a2ae0ef5171eac9fc8191348a3b2d872b6e5e367
Patricia <pa1809ty@gmail.com>
2016-01-12 16:28:38 +0000

    Add providers_as_json method to the models to pass the data to be use by Google maps JS API

--- app/models/contribution.rb
+++ app/models/contribution.rb
@@ -4,4 +4,16 @@ class Contribution < ActiveRecord::Base
   has_many :products
   accepts_nested_attributes_for :products
   validates :collection_date, :collection_time, presence: :true
+
+
+  def self.providers_as_json(providers)
+    providers_json = []
+
+    providers.each do |provider|
+      provider_json = provider.as_json
+      providers_json << provider_json
+    end
+
+    providers_json
+  end
 end
