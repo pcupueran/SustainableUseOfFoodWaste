ebe0c0426d7f7b0c1dfc8d5148e8b125dff2534e
Patricia <pa1809ty@gmail.com>
2016-01-13 15:15:48 +0000

    Add after_signing_inPath_for method in application controller to redirect the user to its profile when successfully signs in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,8 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:organization_name, :email, :password, :password_confirmation, :type)}
   end
+
+  def after_sign_in_path_for(resource)
+    profile_path(resource.profile)
+  end
 end
