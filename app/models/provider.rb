0995716a3a7ff35b9286169bcccc6389aea0d7b8
Patricia <pa1809ty@gmail.com>
2016-01-13 00:40:11 +0000

    Add find_providers method to find providers which have contributions and are in specific addresses

--- app/models/provider.rb
+++ app/models/provider.rb
@@ -1,3 +1,16 @@
 class Provider < User
   has_many :contributions, foreign_key: 'user_id'
+
+  def self.find_providers(providers_with_contributions, addresses)
+    providers = []
+    providers_with_contributions.each do |provider|
+      addresses.each do |address|
+        if provider == address.profile.user
+          providers << address.profile.user
+          break
+        end
+      end
+    end
+    providers
+  end
 end
