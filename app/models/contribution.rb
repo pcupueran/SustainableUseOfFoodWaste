7ab4fff149d8c196e9b98701af4208cd4d94a9e1
Patricia <pa1809ty@gmail.com>
2016-01-13 00:36:24 +0000

    Add validations for nested attributes, the must_have_one_product and collection_date validations

--- app/models/contribution.rb
+++ app/models/contribution.rb
@@ -1,19 +1,29 @@
 class Contribution < ActiveRecord::Base
   has_one :booking
   belongs_to :user
-  has_many :products
-  accepts_nested_attributes_for :products
+  has_many :products, inverse_of: :contribution
+  accepts_nested_attributes_for :products, allow_destroy: :true, reject_if: proc { |attributes| attributes[:product_name].blank? }
   validates :collection_date, :collection_time, presence: :true
-
+  validate :must_have_one_product, :collection_date_validity, :collection_time_validity
 
   def self.providers_as_json(providers)
     providers_json = []
 
     providers.each do |provider|
-      provider_json = provider.as_json
-      providers_json << provider_json
+      providers_json << provider.as_json
     end
-
     providers_json
   end
+
+  def must_have_one_product
+    if products.empty?
+      errors.add(:products, "must have at least one product") if products.empty?
+      self.products << Product.new
+    end
+  end
+
+  def collection_date_validity
+    errors.add(:collection_date, "cannot be in the past") if collection_date < Date.today
+  end
+
 end
