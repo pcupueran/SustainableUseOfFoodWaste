3c1159177c649112ff51c9de3c9a97b1545bdb6c
Patricia <pa1809ty@gmail.com>
2016-01-20 11:12:00 +0000

    Refactor an initializer file for carrierwave gem

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,7 +1,6 @@
 if Rails.env.test? or Rails.env.cucumber?
   CarrierWave.configure do |config|
-    config.
-    storage = :file
+    config.storage = :file
     config.enable_processing = false
   end
 end
